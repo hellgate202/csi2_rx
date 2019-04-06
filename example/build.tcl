0c556309e0a552c3afc8c11dc918042f7742999a
Leonid Ivanov <l.ivanov@metrotek.ru>
2019-04-06 18:08:24 +0300

    succsessfully generated bitstream with build.tcl

--- example/build.tcl
+++ example/build.tcl
@@ -221,3 +221,11 @@ save_constraints -force
 # Run Synthesis
 launch_runs synth_1 -jobs 8
 wait_on_run synth_1
+
+# Run Implementation
+launch_runs impl_1
+wait_on_run impl_1 
+
+# Generate bitstream
+launch_runs impl_1 -to_step write_bitstream -jobs 8
+wait_on_run impl_1
