47614bbec90692d3e74a75f76bdece3e7c16db99
Leonid Ivanov <l.ivanov@metrotek.ru>
2019-04-09 13:24:18 +0300

    added mtstb protect for lp signals

--- src/dphy_settle_ignore.sv
+++ src/dphy_settle_ignore.sv
@@ -4,8 +4,8 @@ module dphy_settle_ignore #(
 )(
   input  clk_i,
   input  srst_i,
-  (* mark_debug = "true" *) input  lp_data_p_i,
-  (* mark_debug = "true" *) input  lp_data_n_i,
+  input  lp_data_p_i,
+  input  lp_data_n_i,
   (* mark_debug = "true" *) output hs_data_valid_o
 );
 
@@ -13,6 +13,26 @@ localparam int IGNORE_TICKS = T_SETTLE / T_CLK;
 localparam int CNT_WIDTH    = $clog2( IGNORE_TICKS );
 
 (* mark_debug = "true" *) logic [CNT_WIDTH - 1 : 0] ignore_cnt;
+logic lp_data_p_d1;
+(* mark_debug = "true" *) logic lp_data_p_d2;
+logic lp_data_n_d1;
+(* mark_debug = "true" *) logic lp_data_n_d2;
+
+always_ff @( posedge clk_i, posedge srst_i )
+  if( srst_i )
+    begin
+      lp_data_p_d1 <= '0;
+      lp_data_p_d2 <= '0;
+      lp_data_n_d1 <= '0;
+      lp_data_n_d2 <= '0;
+    end
+  else
+    begin
+      lp_data_p_d1 <= lp_data_p_i;
+      lp_data_p_d2 <= lp_data_p_d1;
+      lp_data_n_d1 <= lp_data_n_i;
+      lp_data_n_d2 <= lp_data_n_d1;
+    end
 
 (* mark_debug = "true" *)enum logic [2 : 0] { IDLE_S,
                      LP_11_S,
@@ -32,17 +52,17 @@ always_comb
     case( state )
       IDLE_S:
         begin
-          if( lp_data_p_i && lp_data_n_i )
+          if( lp_data_p_d2 && lp_data_n_d2 )
             next_state = LP_11_S;
         end
       LP_11_S:
         begin
-          if( !lp_data_p_i && lp_data_n_i )
+          if( !lp_data_p_d2 && lp_data_n_d2 )
             next_state = LP_01_S;
         end
       LP_01_S:
         begin
-          if( !lp_data_p_i && !lp_data_n_i )
+          if( !lp_data_p_d2 && !lp_data_n_d2 )
             next_state = LP_00_S;
         end
       LP_00_S:
@@ -52,7 +72,7 @@ always_comb
         end
       HS_S:
         begin
-          if( lp_data_p_i && lp_data_n_i )
+          if( lp_data_p_d2 && lp_data_n_d2 )
             next_state = LP_11_S;
         end
     endcase
