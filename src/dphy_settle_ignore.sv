0616884cfc573c43ce2ffeff7c94ba47207250ca
Leonid Ivanov <l.ivanov@metrotek.ru>
2019-04-09 11:45:06 +0300

    added debug probes to settle_ignore

--- src/dphy_settle_ignore.sv
+++ src/dphy_settle_ignore.sv
@@ -4,17 +4,17 @@ module dphy_settle_ignore #(
 )(
   input  clk_i,
   input  srst_i,
-  input  lp_data_p_i,
-  input  lp_data_n_i,
-  output hs_data_valid_o
+  (* mark_debug = "true" *) input  lp_data_p_i,
+  (* mark_debug = "true" *) input  lp_data_n_i,
+  (* mark_debug = "true" *) output hs_data_valid_o
 );
 
 localparam int IGNORE_TICKS = T_SETTLE / T_CLK;
 localparam int CNT_WIDTH    = $clog2( IGNORE_TICKS );
 
-logic [CNT_WIDTH - 1 : 0] ignore_cnt;
+(* mark_debug = "true" *) logic [CNT_WIDTH - 1 : 0] ignore_cnt;
 
-enum logic [2 : 0] { IDLE_S,
+(* mark_debug = "true" *)enum logic [2 : 0] { IDLE_S,
                      LP_11_S,
                      LP_01_S,
                      LP_00_S,
